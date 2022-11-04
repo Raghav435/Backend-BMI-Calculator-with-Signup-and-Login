b9077b02ae5ffb24b9cfd7f507b6fc94d8c5e014
Raghav Shukla <shuklaraghav435@gmail.com>
2022-11-04 12:54:09 +0530

    backend bmi calculator

---
+++ index.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const { connection } = require("./config/db");
+const UserModel = require("./models/UserModel");
+const dotenv = require("dotenv").config();
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
+const { authentication } = require("./middlewares/authentication");
+const BMIModel = require("./models/BMIModel");
+const PORT = process.env.PORT || 8200;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+app.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const isUser = await UserModel.findOne({ email });
+  if (isUser) {
+    res.send({ msg: "User already exists, try logging in" });
+  } else {
+    bcrypt.hash(password, 4, async function (err, hash) {
+      if (err) {
+        res.send("Something went wrong, please try again later");
+      }
+      const new_user = new UserModel({
+        name,
+        email,
+        password: hash,
+      });
+      try {
+        await new_user.save();
+        res.send({ msg: "Sign up successfull" });
+      } catch (err) {
+        res.send({ msg: "Something went wrong, please try again" });
+      }
+    });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({ email });
+  const hashed_password = user.password;
+  const user_id = user._id;
+  console.log(user);
+  console.log(user_id);
+  bcrypt.compare(password, hashed_password, function (err, result) {
+    if (err) {
+      res.send({ msg: "Something went wrong, try again later" });
+    }
+    if (result) {
+      const token = jwt.sign({ user_id }, process.env.SECRET_KEY);
+      res.send({ message: "Login successfull", token });
+    } else {
+      res.send({ msg: "Login failed" });
+    }
+  });
+});
+
+app.get("/getProfile", authentication, async (req, res) => {
+  const { user_id } = req.body;
+  const user = await UserModel.findOne({ _id: user_id });
+  const { name, email } = user;
+  res.send({ name, email });
+});
+
+app.post("/calculateBMI", authentication, async (req, res) => {
+  const { height, weight, user_id } = req.body;
+  const height_in_metre = Number(height) * 0.3048;
+  const BMI = Number(weight) / height_in_metre ** 2;
+  const new_bmi = new BMIModel({
+    BMI,
+    height: height_in_metre,
+    weight,
+    user_id,
+  });
+  await new_bmi.save();
+  res.send({ BMI });
+});
+
+app.get("/getCalculation", authentication, async (req, res) => {
+  const { user_id } = req.body;
+  const all_bmi = await BMIModel.find({ user_id: user_id });
+  res.send({ history: all_bmi });
+});
+
+app.listen(PORT, async () => {
+  try {
+    await connection;
+    console.log("Connected to database succesfully");
+  } catch (err) {
+    console.log("Something error in connection");
+  }
+  console.log(`Listening on ${PORT}`);
+});
